4842fd2ac2ab6c23a2f88afed505bc9a1def12bf
Sidney Jackson <sidneyjackson@Sidneys-iMac.local>
2020-12-04 02:52:34 -0500

    Tried to fill the menu with things. Still a work in progress.

--- clueless_react/src/Masthead.js
+++ clueless_react/src/Masthead.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Masthead.css";
 import "./MenuModals.css";
+var COLORS = require("./game_data/colors.json");
 
 export class Masthead extends React.Component {
   constructor(props) {
@@ -65,8 +66,94 @@ export class Masthead extends React.Component {
       modal.style.display = "none";
     };
 
+    // Note: There is def a much better way to do this...
+
+    const colorSchemeBlue = () => {
+      var masthead = document.getElementById("masthead");
+      masthead.style.background = COLORS.Blue;
+      var dropdownButton = document.getElementsByClassName("dropbtn");
+      for (var i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].style.background = COLORS.Blue;
+      }
+      var dropdownContent = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].style.background = COLORS.Blue + "cb";
+      }
+      //Working on this: .dropdown-content span:hover --> darkColor
+    };
+
+    const colorSchemeRed = () => {
+      var masthead = document.getElementById("masthead");
+      masthead.style.background = COLORS.Red;
+      var dropdownButton = document.getElementsByClassName("dropbtn");
+      for (var i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].style.background = COLORS.Red;
+      }
+      var dropdownContent = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].style.background = COLORS.Red + "cb";
+      }
+      //Working on this: .dropdown-content span:hover --> darkColor
+    };
+
+    const colorSchemeGreen = () => {
+      var masthead = document.getElementById("masthead");
+      masthead.style.background = COLORS.Green;
+      var dropdownButton = document.getElementsByClassName("dropbtn");
+      for (var i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].style.background = COLORS.Green;
+      }
+      var dropdownContent = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].style.background = COLORS.Green + "cb";
+      }
+      //Working on this: .dropdown-content span:hover --> darkColor
+    };
+
+    const colorSchemeYellow = () => {
+      var masthead = document.getElementById("masthead");
+      masthead.style.background = COLORS.Yellow;
+      var dropdownButton = document.getElementsByClassName("dropbtn");
+      for (var i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].style.background = COLORS.Yellow;
+      }
+      var dropdownContent = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].style.background = COLORS.Yellow + "cb";
+      }
+      //Working on this: .dropdown-content span:hover --> darkColor
+    };
+
+    const colorSchemePink = () => {
+      var masthead = document.getElementById("masthead");
+      masthead.style.background = COLORS.Pink;
+      var dropdownButton = document.getElementsByClassName("dropbtn");
+      for (var i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].style.background = COLORS.Pink;
+      }
+      var dropdownContent = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].style.background = COLORS.Pink + "cb";
+      }
+      //Working on this: .dropdown-content span:hover --> darkColor
+    };
+
+    const colorSchemeSilver = () => {
+      var masthead = document.getElementById("masthead");
+      masthead.style.background = COLORS.Silver;
+      var dropdownButton = document.getElementsByClassName("dropbtn");
+      for (var i = 0; i < dropdownButton.length; i++) {
+        dropdownButton[i].style.background = COLORS.Silver;
+      }
+      var dropdownContent = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].style.background = COLORS.Silver + "cb";
+      }
+      //Working on this: .dropdown-content span:hover --> darkColor
+    };
+
     return (
-      <div class="masthead">
+      <div id="masthead" class="masthead">
         <span class="logo">
           <img src={logoImage} />
         </span>
@@ -119,6 +206,24 @@ export class Masthead extends React.Component {
               &times;
             </span>
             <h1>About Us</h1>
+            <p>
+              {" "}
+              In August 2020, the United Coders (A group of student developers
+              enrolled in the Johns Hopkins University’s Foundation of Software
+              Engineering Fall 2020 course) took on a project to reimagine the
+              classic murder mystery board game Clue© as an online-board game
+              called Clue-Less. The end-goal of this project was to create a
+              product that offered an engaging and intuitive online board game
+              experience that was highly accessible and easy to use all while
+              maintaining the basic rules and game mechanics of the original
+              board game Clue©. The final product of this effort is this web
+              application, "Clue-Less" by the United Coders.
+            </p>
+            <p>
+              {" "}
+              Developers: Sophia Anousheh • Kathryn Crisafulli • Sidney Jackson
+              • Cheryl Limer • Erik Riis{" "}
+            </p>
           </div>
         </div>
 
@@ -128,6 +233,28 @@ export class Masthead extends React.Component {
               &times;
             </span>
             <h1>Game Settings</h1>
+            <p class="settings"> Select Color Scheme: </p>
+            <div class="row">
+              <div class="column" onClick={colorSchemeBlue}>
+                <img src="./SettingColors/BlueColor.png" width="25" />
+              </div>
+              <div class="column" onClick={colorSchemeRed}>
+                <img src="./SettingColors/RedColor.png" width="25" />
+              </div>
+              <div class="column" onClick={colorSchemeGreen}>
+                <img src="./SettingColors/GreenColor.png" width="25" />
+              </div>
+              <div class="column" onClick={colorSchemeYellow}>
+                <img src="./SettingColors/YellowColor.png" width="25" />
+              </div>
+              <div class="column" onClick={colorSchemePink}>
+                <img src="./SettingColors/PinkColor.png" width="25" />
+              </div>
+              <div class="column" onClick={colorSchemeSilver}>
+                <img src="./SettingColors/SilverColor.png" width="25" />
+              </div>
+              <div> </div>
+            </div>
           </div>
         </div>
       </div>
